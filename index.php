49584562ce3cb7feceb22e01f9f786f9f904f106
Tayler R Mandel <tmandel@ampersand.seas.upenn.edu>
2013-04-04 00:05:44 -0400

    tmandel - some style

--- index.php
+++ index.php
@@ -2,14 +2,24 @@
   <head>
     <title>TATE'S SITE</title>
   </head>
+  <style>
+    div {
+    border:2px solid #000000;
+    padding:10px 40px; 
+    background-color:red;
+    width:300px;
+    border-radius:25px;
+    box-shadow: 10px 10px 5px #888888
+    }
+   </style>
   <body>
-    <t>Number of visitors: </t>
+    <t><strong>Number of visitors: </strong></t>
     <?php 
        system ('python getcount.py');
        system ('python increment.py'); 
     ?>
-    <t><br/><br/>Users by browser:<br/></t>
-    <div style="margin-left:15px;">
+    <t><br/><strong>Users by browser:</strong><br/></t>
+    <div>
     <?php 
        $a = $_SERVER['HTTP_USER_AGENT'];
        $command = "python browser.py \"$a\"";
